dc958cad9e3e2d806d22c6f794e7438841ee3b76
farhan alwanda <raja.farhan.alwanda@gmail.com>
2025-11-12 17:26:58 +0700

    fix theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    // Baris inilah yang paling penting
-    // Ini memberitahu ekstensi VS Code untuk
-    // memindai SEMUA file .html di proyek Anda
+
     content: [
         "./**/*.html"
     ],
 
-    // Bagian ini bisa Anda kosongkan
-    // karena konfigurasi aslinya Anda atur di CDN
     theme: {
         extend: {},
     },
